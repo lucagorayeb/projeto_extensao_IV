077933629f3d4eae767b16428e523ab2591d13f7
Luca Gorayeb <lucagorayeb@gmail.com>
2026-09-21 23:27:30 -0400

    -

--- app/backend/database/file.sql
+++ app/backend/database/file.sql
@@ -2,49 +2,50 @@
 PRAGMA foreign_keys = ON;
 
 CREATE TABLE disciplinas(
-    id_disciplina INTEGER PRIMARY KEY AUTO_INCREMENT,
+    id_disciplina INTEGER PRIMARY KEY AUTOINCREMENT,
     nome_disciplina TEXT NOT NULL,
     fk_formato_disciplina INT NOT NULL,
     carga_horaria_disciplina TEXT NOT NULL,
     horario_inicio_disciplina TIME NOT NULL,
     horario_termino_disciplina TIME NOT NULL,
-    pratica INT NOT NULL, -- Simulando um bool com 0 ou 1.
-    especialidade_necessaria_disciplina TEXT NOT NULL,
-    FOREIGN KEY (fk_formato) REFERENCES formato(id_formato) 
+    pratica_disciplina INT NOT NULL, -- Simulando um bool com 0 ou 1.
+    especialidade_disciplina TEXT NOT NULL,
+    FOREIGN KEY (fk_formato_disciplina) REFERENCES formato(id_formato) 
 );
 
 CREATE TABLE formatos(
-    id_formato INTEGER PRIMARY KEY AUTO_INCREMENT,
+    id_formato INTEGER PRIMARY KEY AUTOINCREMENT,
     nome_formato TEXT NOT NULL
 );
 
 CREATE TABLE professores(
-    id_professores INTEGER PRIMARY KEY AUTO_INCREMENT,
+    id_professores INTEGER PRIMARY KEY AUTOINCREMENT,
     nome_professores TEXT NOT NULL,
     especialidade_professores TEXT NOT NULL
 );
 
 CREATE TABLE salas(
-    id_salas INTEGER PRIMARY KEY AUTO_INCREMENT,
+    id_salas INTEGER PRIMARY KEY AUTOINCREMENT,
     numero_nome_sala TEXT NOT NULL,
+    limite_alunos_sala INT NOT NULL,
     fk_tipo_sala INT NOT NULL,
     FOREIGN KEY (fk_tipo_sala) REFERENCES tipo_salas(id_tipo_sala)
 );
 
 CREATE TABLE tipo_salas(
-    id_tipo_sala INTEGER PRIMARY KEY AUTO_INCREMENT,
+    id_tipo_sala INTEGER PRIMARY KEY AUTOINCREMENT,
     tipo_sala TEXT NOT NULL
 );
 
 CREATE TABLE dados_usuarios(
-    id_dados_usuario INTEGER PRIMARY KEY AUTO_INCREMENT,
+    id_dados_usuario INTEGER PRIMARY KEY AUTOINCREMENT,
     nome_dados_usuario TEXT NOT NULL,
     data_nascimento_dados_usuario DATE NOT NULL,
     cpf_dados_usuario TEXT NOT NULL
 );
 
 CREATE TABLE usuarios(
-    id_usuario INTEGER PRIMARY KEY AUTO_INCREMENT,
+    id_usuario INTEGER PRIMARY KEY AUTOINCREMENT,
     email_usuario TEXT NOT NULL,
     senha_usuario TEXT NOT NULL,
     fk_dados_usuario INT NOT NULL,
--- app/backend/repository/disciplinas_repository.py
+++ app/backend/repository/disciplinas_repository.py
@@ -1,37 +1,67 @@
-# from sqlalchemy import text
+from sqlalchemy import text
 from .base_repository import BaseRepository as BR
+from typing import Any
 
 
 class DisciplinaRepository(BR):
 
-    def insert(self, item: str) -> None:
-        # stmt = text("""
-        # INSERT INTO disciplinas
-        # (
-        #     disciplina,
-        #     fk_formato_disciplina
-        # )
-        # VALUES
-        # (
-        #     :item
-        # );""")
-        # data = {'item': item}
-        # self.execute_and_commit_db_query(stmt, data)
-        print("Chegou até a função insert da tabela disciplina.")
+    def insert(self, data: dict[Any]) -> None:
+        stmt = text("""
+        INSERT INTO disciplinas
+        (
+            nome_disciplina,
+            fk_formato_disciplina,
+            carga_horaria_disciplina,
+            horario_inicio_disciplina,
+            horario_termino_disciplina,
+            pratica_disciplina,
+            especialidade_necessaria_disciplina
+        )
+        VALUES
+        (
+            :nome_disciplina,
+            :fk_formato_disciplina,
+            :carga_horaria_disciplina,
+            :horario_inicio_disciplina,
+            :horario_termino_disciplina,
+            :pratica_disciplina,
+            :especialidade_necessaria_disciplina
+        );""")
+        self.execute_and_commit_db_query(stmt, data)
 
     def select(self) -> list[tuple]:
-        # stmt = text("""
-        # SELECT
-        #     id,
-        #     disciplina,
-        #     fk_formato_disciplina
-        # FROM
-        #     disciplinas;""")
-        # return self.execute_db_query(stmt)
-        print("Chegou até a função select da tabela disciplina.")
+        stmt = text("""
+        SELECT
+            d.id_disciplina,
+            d.nome_disciplina,
+            d.fk_formato_disciplina,
+            f.nome_formato,
+            d.carga_horaria_disciplina,
+            d.horario_inicio_disciplina,
+            d.horario_termino_disciplina,
+            d.pratica_disciplina,
+            d.especialidade_necessaria_disciplina
+        FROM
+            disciplinas AS d
+        LEFT JOIN formato AS f ON f.id_formato = d.fk_formato_disciplina;
+        """)
+        return self.execute_db_query(stmt)
 
-    def update(self, data: dict, id: int) -> None:
-        print("Chegou até a função update da tabela disciplina.")
+    def update(self, data: dict[Any], id: int) -> None:
+        stmt = text("""
+        UPDATE disciplinas
+            SET nome_disciplina = :nome,
+                fk_formato_disciplina = :formato,
+                carga_horaria_disciplina = :carga_horaria,
+                horario_inicio_disciplina = :horario_inicio,
+                horario_termino_disciplina = :horario_termino,
+                pratica_disciplina = :pratica,
+                especialidade_necessaria_disciplina = :especialidade
+            WHERE
+                id_disciplina = :id,
+            """)
+        self.execute_and_commit_db_query(stmt, data, id)
 
     def delete(self, id: int) -> None:
-        print("Chegou até a função delete da tabela disciplina.")
+        stmt = text("""DELETE FROM disciplinas WHERE id = :id;""")
+        self.execute_and_commit_db_query(stmt, None, id)
