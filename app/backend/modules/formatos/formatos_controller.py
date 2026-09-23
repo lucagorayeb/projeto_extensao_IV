626f24788f8c6eed6364a84fba6f4df60d990ef6
Luca Gorayeb <lucagorayeb@gmail.com>
2026-09-23 11:39:25 -0400

    feat: Adding the update and delete method.
    Changing the name of the variable to be more clear.

--- app/backend/modules/formatos/formatos_controller.py
+++ app/backend/modules/formatos/formatos_controller.py
@@ -1,12 +1,20 @@
-from formatos_service import FormatoService as fs
+from formatos_service import FormatoService as serv
 
 
 class FormatoController:
 
     @staticmethod
     def select_formato_controller():
-        fs.select_formato_service()
+        serv.select_formato_service()
 
     @staticmethod
     def insert_formato_controller():
-        fs.get_from_formatos_service()
+        serv.insert_formato_service()
+
+    @staticmethod
+    def update_formato_controller():
+        serv.update_formato_service()
+
+    @staticmethod
+    def delete_formato_controller():
+        serv.delete_formato_service()
