026f4fcf1af8cb868267bb5c0ba38ef2b0a2a867
Luca Gorayeb <lucagorayeb@gmail.com>
2026-09-23 11:23:35 -0400

    feat: Adding a new engine variable. Engine for MySQL were added.

--- app/backend/core/interface.py
+++ app/backend/core/interface.py
@@ -1,7 +1,23 @@
 from sqlalchemy import create_engine
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 
-engine = create_engine(
+# Conexão com o SQLITE
+engine_sqlite = create_engine(
     "sqlite+pysqlite:///database/ensalamento.sqlite",
     echo=False
 )
+
+# Conexão com o MySQL
+usuario = os.getenv("MYSQL_USER")
+senha = os.getenv("MYSQL_PASSWORD")
+host = os.getenv("MYSQL_HOST")
+porta = os.getenv("MYSQL_PORT")
+banco = os.getenv("MYSQL_DATABASE")
+
+string_conexao_mysql = f"""
+    mysql+pymysql://{usuario}:{senha}@{host}:{porta}/{banco}"""
+
+engine_mysql = create_engine(string_conexao_mysql)
