7c16251d7923301a91963705be68a0c262226d26
Luca Gorayeb <lucagorayeb@gmail.com>
2026-09-24 13:02:39 -0400

    err: It has a error with the engine of the database. It will be corrected soon.

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
-from app.backend.modules.disciplinas.disciplinas_controller import (
-    DisciplinasController as dc
-)
-from app.backend.modules.formatos.formatos_controller import (
-    FormatosController as fc
-)
+# from app.backend.modules.disciplinas.disciplinas_controller import (
+#     DisciplinasController as dc
+# )
+# from app.backend.modules.formatos.formatos_controller import (
+#     FormatosController as fc
+# )
+from scripts.insertions.insert_formatos import (
+    insert_formatos
+)   
 
 
-try:
-    dc.slct_disciplinas_controller()
-except Exception as e:
-    print(f"Erro: {e}")
+# try:
+#     dc.slct_disciplinas_controller()
+# except Exception as e:
+#     print(f"Erro: {e}")
 
-try:
-    data = {"nome": "TESTE"}
-    fc.insrt_formatos_controller(data)
-except Exception as e:
-    print(f"Erro: {e}")
+# try:
+#     data = {"nome": "TESTE"}
+#     fc.insrt_formatos_controller(data)
+# except Exception as e:
+#     print(f"Erro: {e}")
+
+insert_formatos()
